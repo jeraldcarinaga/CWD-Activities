01ff702cc1221bbffc13f43856c97ed06803ffd9
jeraldcarinaga <jjeraldcarinaga@gmail.com>
2022-11-19 08:38:13 +0800

    First Commit

---
+++ Drafts/index.js
@@ -0,0 +1,24 @@
+/*
+document.getElementById("submitbtn").onclick = function(){
+    const x = document.getElementById("input").value;
+
+    if(x%2 == 0){
+        console.log("even");
+    }
+    else{
+        console.log("odd");
+    }
+}
+
+
+x = 4
+
+if(x%2 == 0){
+    console.log("even")
+}
+else{
+    console.log("odd")
+}
+*/
+
+
